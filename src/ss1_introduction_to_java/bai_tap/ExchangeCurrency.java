d3905408bff9960e8370fcd7c8643cfc5499e41c
nguyentienquan408 <Nguyentienquan408@gmail.com>
2025-05-09 07:10:27 +0700

    thuchanh

--- src/ss1_introduction_to_java/bai_tap/ExchangeCurrency.java
+++ src/ss1_introduction_to_java/bai_tap/ExchangeCurrency.java
@@ -7,8 +7,9 @@ public class ExchangeCurrency {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhap tien USD ");
+        final int RATE = 25500;
         double USD = sc.nextDouble();
-        double VND = USD*25500;
+        double VND = USD*RATE  ;
         Locale vietnam = new Locale("vi", "VN");
         NumberFormat vnFormat = NumberFormat.getCurrencyInstance(vietnam);
         System.out.println("VND "+ vnFormat.format(VND));
---
+++ src/ss2_vong_lap/thuc_hanh/SoNguyenTo.java
@@ -0,0 +1,24 @@
+package ss2_vong_lap.thuc_hanh;
+
+
+import java.util.Scanner;
+
+public class SoNguyenTo {
+    public static boolean soNguyenTo(int n){
+        if(n<2){
+            return false;
+        }else{
+            for (int i=2 ; i<= Math.sqrt(n);i++){
+                if(n%i==0) return false;
+
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so");
+        int n = sc.nextInt();
+        System.out.println(soNguyenTo(n));
+    }
+}
